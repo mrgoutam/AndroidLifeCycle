023ea04abcedf14f44ff9661bab1323628ed2cd5
goutamstark2 <31756722+goutamstark2@users.noreply.github.com>
2019-03-29 10:05:03 +0530

    Project Completed

--- AndroidLifeCycle/app/src/main/java/stark/com/androidlifecycle/MainActivity.java
+++ AndroidLifeCycle/app/src/main/java/stark/com/androidlifecycle/MainActivity.java
@@ -2,12 +2,66 @@ package stark.com.androidlifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //called when activity is first created.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onCreate invoked");
+        Toast.makeText(this, "onCreate invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called when activity is becoming visible to the user.
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+        Toast.makeText(this, "onStart invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called when activity will start interacting with the user.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+        Toast.makeText(this, "onResume invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called when activity is not visible to the user.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+        Toast.makeText(this, "onPause invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called when activity is no longer visible to the user.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+        Toast.makeText(this, "onStop invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called after your activity is stopped, prior to start.
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+        Toast.makeText(this, "onRestart invoked", Toast.LENGTH_SHORT).show();
+    }
+
+    //called before the activity is destroyed.
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+        Toast.makeText(this, "onDestroy invoked", Toast.LENGTH_SHORT).show();
     }
 }
